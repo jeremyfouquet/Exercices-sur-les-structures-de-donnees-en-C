5e709c0421b28a3d231a67141b1dc74f351c67ee
jeremyfouquet <jeremy.fouquet@outlook.com>
2022-03-31 01:43:17 +0200

    ExoREV1Rigides
    A1

--- main.c
+++ main.c
@@ -8,10 +8,14 @@
 #include <stdio.h> // puts(), printf()
 #include <stdlib.h> // exit()
 #include <string.h> // strlen()
+
 #define str char *
 
 void exercice_A_1(void) ; // prototyp
-void lecture_float(const str, const float[]) ; // prototyp
+void lectureF(const str, const int, const char format[2], const str type) ; // prototyp
+void lectureD(const str, const int, const char format[2], const str) ; // prototyp
+void lectureI(const str, const int, const char format[2], const str) ; // prototyp
+void lectureL(const str, const int, const char format[2], const str) ; // prototyp
 int compte_mots(const str) ; // prototyp
 void usage(const str, const str) ; // prototyp
 
@@ -19,50 +23,118 @@ void usage(const str, const str) ; // prototyp
 
 int main(int k, const str ldc[], const str envp[]) {
     printf("%s\n", "A. Exercices sur les structures de données");
+    puts("");
     exercice_A_1();
     return 0 ;
 }
 
 void exercice_A_1(void) {
     printf("%s\n", "A.1 Vecteurs");
+    puts("");
     printf("%s\n", "Écrivez un programme qui lit une suite de nombres dans un fichier et les met dans un vecteur (une table). Affichez chaque nombre avec son indice dans la table. On suppose connue à l’avance la taille de la suite. Exemple : le fichier contient « 21<CR> 23<CR> 275.4<CR>... ». <CR> symbolise un retour-charriot). Le programme affiche :");
+    puts("");
     int tailleSuite = 6;
     str fichier = "fichier.txt";
-    float monVecteur[tailleSuite] ;
-    lecture_float(fichier, monVecteur) ;
+    lectureF(fichier, tailleSuite, "%f", "Float") ;
+    lectureD(fichier, tailleSuite, "%lf", "Double") ;
+    lectureI(fichier, tailleSuite, "%d", "Integer") ;
+    lectureL(fichier, tailleSuite, "%ld", "Long") ;
 }
 
-void lecture_float(const str filename, const float monVecteur[]) {
-    int nb_mots = compte_mots(filename) ;
-    FILE * R = fopen(filename, "r") ; // ouvre le canal R en mode "r" pour ldc [1]
-    if (! R) usage(filename, "Fichier inconnu") ; // si pas de fichier message erreur exit
-    str mots[nb_mots] ;
-    int lu, max ;
-    for (max = 0 ; max < nb_mots ; max++) {
-        char sas[32] ;
-        lu = fscanf(R, "%s", sas) ;
-        mots[max] = strdup(sas) ;
-    }
-    max = 0 ;
-    while(mots[max] && max < nb_mots) {
-        printf("%s\n", mots[max++]) ;
-    }
-    fclose(R) ; // ferme le canal
-}
-
-int compte_mots(const str filename) {
+void lectureF(const str filename, const int taille, const char format[2], const str type) {
+    float monVecteur[taille] ;
+    printf ("Type : %s\n", type) ;
     FILE * R = fopen(filename, "r") ; // ouvre le canal R en mode "r" pour le fichier avec le nom filename
-    if (! R) usage(filename, "Fichier inconnu") ; // si pas de fichier appel usage(const str, const str)
-    char stop[32] ;
-    int scan, nb_mots = 0 ;
-    while((scan = fscanf(R, "%s", stop)) != EOF) {
-        if (scan) nb_mots++ ;
-        else usage(filename, "Le fichier contier des caractères non autorisés") ;
+    if (! R) usage(filename, "Fichier non lisible") ; // si pas de fichier appel usage(const str, const str)
+    float valeur, lu = 0;
+    str cr;
+    str formarSrt = "%s";
+    for (int max = 0 ; max < taille && EOF != lu ; ++max) {
+        lu = fscanf(R, format, & valeur) ;
+        if (! lu) usage(filename, "Le fichier ne contient pas uniquement une suite de nombres.") ;
+        fscanf(R, formarSrt, & cr) ;
+        monVecteur[max] = valeur;
     }
-    fclose(R) ; // ferme le canal
-    return nb_mots ;
+    char indice[8] = "%d. ";
+    strcat(indice, format);
+    for (int i = 0 ; i < taille ; i++) {
+        printf (indice, i+1, monVecteur[i]) ;
+        puts("");
+    }
+    fclose(R); // ferme le canal
+    puts("");
+}
+void lectureD(const str filename, const int taille, const char format[2], const str type) {
+    double monVecteur[taille] ;
+    printf ("Type : %s\n", type) ;
+    FILE * R = fopen(filename, "r") ; // ouvre le canal R en mode "r" pour le fichier avec le nom filename
+    if (! R) usage(filename, "Fichier non lisible") ; // si pas de fichier appel usage(const str, const str)
+    double valeur, lu = 0;
+    str cr;
+    str formarSrt = "%s";
+    for (int max = 0 ; max < taille && EOF != lu ; ++max) {
+        lu = fscanf(R, format, & valeur) ;
+        if (! lu) usage(filename, "Le fichier ne contient pas uniquement une suite de nombres.") ;
+        fscanf(R, formarSrt, & cr) ;
+        monVecteur[max] = valeur;
+    }
+    char indice[8] = "%d. ";
+    strcat(indice, format);
+    for (int i = 0 ; i < taille ; i++) {
+        printf (indice, i+1, monVecteur[i]) ;
+        puts("");
+    }
+    fclose(R); // ferme le canal
+    puts("");
 }
 
+void lectureI(const str filename, const int taille, const char format[2], const str type) {
+    int monVecteur[taille] ;
+    printf ("Type : %s\n", type) ;
+    FILE * R = fopen(filename, "r") ; // ouvre le canal R en mode "r" pour le fichier avec le nom filename
+    if (! R) usage(filename, "Fichier non lisible") ; // si pas de fichier appel usage(const str, const str)
+    int valeur, lu = 0;
+    str cr;
+    str formarSrt = "%s";
+    for (int max = 0 ; max < taille && EOF != lu ; ++max) {
+        lu = fscanf(R, format, & valeur) ;
+        if (! lu) usage(filename, "Le fichier ne contient pas uniquement une suite de nombres.") ;
+        fscanf(R, formarSrt, & cr) ;
+        monVecteur[max] = valeur;
+    }
+    char indice[8] = "%d. ";
+    strcat(indice, format);
+    for (int i = 0 ; i < taille ; i++) {
+        printf (indice, i+1, monVecteur[i]) ;
+        puts("");
+    }
+    fclose(R); // ferme le canal
+    puts("");
+}
+
+void lectureL(const str filename, const int taille, const char format[2], const str type) {
+    long monVecteur[taille] ;
+    printf ("Type : %s\n", type) ;
+    FILE * R = fopen(filename, "r") ; // ouvre le canal R en mode "r" pour le fichier avec le nom filename
+    if (! R) usage(filename, "Fichier non lisible") ; // si pas de fichier appel usage(const str, const str)
+    long valeur, lu = 0;
+    str cr;
+    str formarSrt = "%s";
+    for (int max = 0 ; max < taille && EOF != lu ; ++max) {
+        lu = fscanf(R, format, & valeur) ;
+        if (! lu) usage(filename, "Le fichier ne contient pas uniquement une suite de nombres.") ;
+        fscanf(R, formarSrt, & cr) ;
+        monVecteur[max] = valeur;
+    }
+    char indice[8] = "%d. ";
+    strcat(indice, format);
+    for (int i = 0 ; i < taille ; i++) {
+        printf (indice, i+1, monVecteur[i]) ;
+        puts("");
+    }
+    fclose(R); // ferme le canal
+    puts("");
+}
 void usage(const str P, const str D) {
     printf("Provenance de l'ERREUR : %s\n", P);
     printf("Détails de l'ERREUR: %s\n", D);
